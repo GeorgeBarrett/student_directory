c40552a960e85e8bd66927e0e5714f0c48297e82
george <georgeaustinbarrett@gmail.com>
2023-03-30 14:39:31 +0100

    refactored code using a variable and string interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -12,4 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
-print "Overall, we have\n11\ngreat students\n"
+puts "Overall, we have #{student_count} great students"
