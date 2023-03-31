8b5c689dff3063573cb04c786c1085dcf2b42107
george <georgeaustinbarrett@gmail.com>
2023-03-31 16:11:32 +0100

    code tidied. while loop for printing method still needs to be written

---
+++ step8_exercise5.rb
@@ -0,0 +1,39 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+    
+  students = []
+    
+  name = gets.chomp
+    
+  while !name.empty? do
+    
+    students << {name: name, cohort: :november, hobbies: :crime}
+    puts "Now we have #{students.count} students"
+    
+    name = gets.chomp    
+  end  
+  students  
+end
+    
+def print_header
+  puts "The students of Villains Academy" 
+  puts "-------------"
+end
+    
+def print(students)
+  students.each do |x|
+    if x[:name].length < 12 
+      puts "#{x[:name]} (#{x[:cohort]} cohort)"
+    end
+  end
+end
+      
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+    
+students = input_students
+print_header
+print(students)
+print_footer(students)
