64f8ed71b4b3bb286e5fac58b59bd6e4e81d0453
george <georgeaustinbarrett@gmail.com>
2023-03-31 13:32:28 +0100

    step8 challenge1 complete. use .each.with_index to give an index to the students

--- step8_exercise1.rb
+++ step8_exercise1.rb
@@ -1,37 +1,24 @@
-def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-  
-    students = []
-  
-    name = gets.chomp
-  
-    while !name.empty? do
-  
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-  
-      name = gets.chomp
-    end
-    students  
-  end
-  
-  def print_header
-    puts "The students of Villains Academy" 
-    puts "-------------"
-  end
-  
-  def print(students)
-    students.each_with_index(1) do |x|
-      puts "#{x[:name]} (#{x[:cohort]} cohort)"
-    end
-  end
-  
-  def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
-  end
-  
-  students = input_students
-  print_header
-  print(students)
-  print_footer(students)
+student_count = 11
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
+puts "The students of Villains Academy"
+puts "-------------"
+
+students.each.with_index(1) do |student, index|
+  puts "#{student} #{index}"
+end
+
+puts "Overall, we have #{student_count} great students"
