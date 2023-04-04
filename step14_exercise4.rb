1033cfca35889ac3c1cc70d719462c210b15086c
george <georgeaustinbarrett@gmail.com>
2023-04-04 12:52:52 +0100

    i added the try_load_students method (this has been renamed to something better in a later exercise). this method ensures that the file exists and loads it. i also invoked the try_load_students method and interactive_menu method at the bottom

--- step14_exercise4.rb
+++ step14_exercise4.rb
@@ -1,52 +1,54 @@
 @students = []
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp 
+  while !name.empty? do   
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"  
+    name = gets.chomp
+  end
+  puts "Your students have been added"
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(STDIN.gets.chomp)
+    user_options(gets.chomp)
   end
 end
 
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit 
-  when "3"
-    save_students
-  when "4"
-    load_students
-  else
-    puts "I don't know what you meant, try again"
-  end
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
-  end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to student.csv"
+  puts "4. To load the student.csv file"
+  puts "9. Exit" 
 end
 
 def show_students
   print_header
   print_student_list
   print_footer
+end
+
+def user_options(selection)
+  case selection
+  when "1"
+    puts ""
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit 
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -71,29 +73,29 @@ def save_students
     csv_line = student_data.join(",")
     file.puts csv_line
   end
-  file.close
+  puts "The students have been saved to students.csv"
+  file.close 
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+  file = File.open("students.csv", "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
+    name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
 
-def try_load_students
+def find_file
   filename = ARGV.first
-  return if filename.nil? 
-  if File.exists?(filename)
+  return if filename.nil?
+  if File.exist?(filename)
     load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
-  else 
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Loaded #{students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} does not exist"
     exit
   end
 end
 
-try_load_students
 interactive_menu
